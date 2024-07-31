f1013354a4cd1893d19cb156f4a4130bbe735d92
Vasukiuma1998-xxx <vasukijegan28@gmail.com>
2024-07-31 12:34:33 +0530

    pushing into git

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './DescriptionBox.css'
+const DescriptionBox=()=>{
+    return(
+            <div className="descriptionbox">
+                <div className="descriptionbox-navigator">
+                    <div className="descriptionbox-nav-box">Description</div>
+                    <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+
+                </div>
+                <div className="descriptionbox-description">
+                    <p>Our e-commerce website is a vibrant online marketplace offering a diverse range of stylish and high-quality dresses for men, women, and kids. Catering to various tastes and occasions, the site features an extensive collection that includes casual wear, formal attire, and everything in between. With a user-friendly interface, secure payment options, and efficient customer service, our website ensures a seamless shopping experience for all fashion enthusiasts.</p>
+                </div>
+            </div>
+    )
+}
+
+export default DescriptionBox
